a41714328f12ad22673207a250bbe6add54902c6
tuvl1990 <tulv1990@gmail.com>
2017-01-04 22:29:47 +0700

    ApiYoutube

---
+++ app/src/main/java/tulv/apiyoutube/Config.java
@@ -0,0 +1,13 @@
+package tulv.apiyoutube;
+
+/**
+ * Created by tulv on 1/4/2017.
+ */
+public class Config {
+    // Google Console APIs developer key
+    // Replace this key with your's
+    public static final String DEVELOPER_KEY = "AIzaSyDISxznNty6NtermfAXgu8s970x5eVTTWQ";
+
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO_CODE = "a4NT5iBFuZs";
+}
